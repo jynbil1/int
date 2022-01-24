28e75627229fc92a4e84bc9254165ce95c3155f9
George <george@hanpoom.com>
2022-01-24 09:04:05 +0900

    [2022-01-24 09:04] Json File cannot read error fixed.

--- src/main/java/hanpoom/internal_cron/utility/shipment/dhl/config/DHLShipmentStatusCode.java
+++ src/main/java/hanpoom/internal_cron/utility/shipment/dhl/config/DHLShipmentStatusCode.java
@@ -1,37 +1,35 @@
 package hanpoom.internal_cron.utility.shipment.dhl.config;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 @Service
 public class DHLShipmentStatusCode {
     public DHLShipmentStatusCode() {
 
     }
-    private final static String JSON_FILE = "classpath:properties/dhl/shipment/status.json";
+
+    private final static String JSON_FILE = "/properties/dhl/shipment/status.json";
 
     public JSONObject getShipmentStatusJSON() throws IOException {
-        FileInputStream fis = null;
-        InputStreamReader isr = null;
-        try {
-            fis = new FileInputStream(ResourceUtils.getFile(JSON_FILE));
-            isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-            JSONTokener tokener = new JSONTokener(isr);
+        try (InputStream in = getClass().getResourceAsStream(JSON_FILE);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+            JSONTokener tokener = new JSONTokener(reader);
             return new JSONObject(tokener);
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            fis.close();
         }
         return null;
     }
