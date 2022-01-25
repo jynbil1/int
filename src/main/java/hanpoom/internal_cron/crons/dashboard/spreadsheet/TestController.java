72fb1a4c2ffca02b7fa9ceb16c08712b90e548c8
George <george@hanpoom.com>
2022-01-25 15:22:46 +0900

    [2022-01-25 15:22] Test restored.

--- src/main/java/hanpoom/internal_cron/crons/dashboard/spreadsheet/TestController.java
+++ src/main/java/hanpoom/internal_cron/crons/dashboard/spreadsheet/TestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import hanpoom.internal_cron.crons.dashboard.spreadsheet.service.GlobalDashboardService;
 import hanpoom.internal_cron.crons.dashboard.spreadsheet.service.SpreadSheetCRUDService;
 import hanpoom.internal_cron.utility.calendar.service.CalendarService;
+import lombok.Getter;
 
 @RestController
 public class TestController {
@@ -26,6 +27,12 @@ public class TestController {
         this.calendar = calendar;
     }
 
+    private final static String SPREADSHEET_ID = "114n3w9q8ytp0z5zFoiOo1xg_cP2nt3yspYKQJvT1KuU";
+    private final static String SHEET = "22 GLOBAL_DASHBOARD";
+    private final static int SHEET_ID = 1110823798;
+    public final String STARTING_COLUMN = "D";
+    public final int STARTIIG_ROW = 5;
+
     // "0 0 * * * *" = the top of every hour of every day.
     // "* * * * * *" = 매초 실행 합니다.
     // "*/10 * * * * *" = 매 10초마다 실행한다.
@@ -36,67 +43,71 @@ public class TestController {
     // "0 0 9-17 * * MON-FRI" = 오전 9시부터 오후 5시까지 주중(월~금)에 실행한다.
     // "0 0 0 25 12 ?" = every Christmas Day at midnight
 
-    // 매주 월요일 자정이 되면 수행.
-    // @GetMapping("/test")
-    // public String testPage() {
-    //     String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern(CalendarService.DATE_TIME_FORMAT_PATTERN));
-    //     System.out.println(now + " 에 작업을 수행함 -- /test 임");
+    @GetMapping("/gtest")
+    public void getSpreadSheetTest() {
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern(CalendarService.DATE_TIME_FORMAT_PATTERN));
+        System.out.println(now + " 에 작업을 수행함");
 
-    //     // 데이터를 가져온다.
-    //     // 1. 매출
-    //     String revenue = dashboard.getLastWeekRevenue();
-    //     // 2. 유저 수
-    //     // 2.1. 신규 가입자
-    //     String newCustomers = dashboard.getLastWeekNewUsers();
-    //     // 2.2. 신규 가입자 중 기간내 신규 구매자
-    //     String newPurchasers = dashboard.getLastWeekNewPurchase();
-    //     // 2.3. 신규 가입자 중 기간 내 재구매
-    //     String newRepurchasers = dashboard.getLastWeekRePurchase();
+        // 데이터를 가져온다.
+        // 1. 매출
+        String revenue = dashboard.getLastWeekRevenue();
+        // 2. 유저 수
+        // 2.1. 신규 가입자
+        String newCustomers = dashboard.getLastWeekNewUsers();
+        // 2.2. 신규 가입자 중 기간내 신규 구매자
+        String newPurchasers = dashboard.getLastWeekNewPurchase();
+        // 2.3. 신규 가입자 중 기간 내 재구매
+        String newRepurchasers = dashboard.getLastWeekRePurchase();
 
-    //     // 3. 주문건 수
-    //     String totalOrders = dashboard.getLastWeekTotalOrders();
+        // 3. 주문건 수
+        String totalOrders = dashboard.getLastWeekTotalOrders();
 
-    //     // 4. 상품 마진
-    //     String totalMargins = dashboard.getLastWeekMargins();
+        // 4. 상품 마진
+        String totalMargins = dashboard.getLastWeekMargins();
 
-    //     // C 컬럼은 주 차를 뜻한다. 주 차가 제일 높은 값을 가져온다.
-    //     // 가져온 값 중 숫자가 없으면 1 부터 시작한다.
-    //     int maxInt = 0;
-    //     List<List<Object>> currentCContent = spreadSheet.getContents("C:C");
-    //     for (List<Object> data : currentCContent) {
-    //         for (Object datum : data) {
-    //             String strValue = String.valueOf(datum);
-    //             if (strValue.length() > 0) {
-    //                 try {
-    //                     int intVal = Integer.parseInt(strValue);
-    //                     if (maxInt < intVal) {
-    //                         maxInt = intVal;
-    //                     } else {
-    //                         continue;
-    //                     }
-    //                 } catch (NumberFormatException nfe) {
-    //                     continue;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // 가져온 값 더하기 1을 한다.
-    //     maxInt += 1;
+        spreadSheet.setSheet(SHEET);
+        spreadSheet.setSheetID(SHEET_ID);
+        spreadSheet.setSpreadSheetID(SPREADSHEET_ID);
+        spreadSheet.setStartingColumn(STARTING_COLUMN);
+        spreadSheet.setStartingRow(STARTIIG_ROW);
 
-    //     // 가져온 데이터를 엑셀 시트에 기입한다.
-    //     List<Object> dataSet = Arrays
-    //             .asList(calendar.getPreviousWeekMonday(false),
-    //                     calendar.getPreviousWeekSunday(false),
-    //                     String.valueOf(maxInt), revenue,
-    //                     newCustomers, newPurchasers, newRepurchasers,
-    //                     totalOrders, totalMargins);
-    //     boolean isInserted = spreadSheet.insertRow(dataSet);
+        // C 컬럼은 주 차를 뜻한다. 주 차가 제일 높은 값을 가져온다.
+        // 가져온 값 중 숫자가 없으면 1 부터 시작한다.
+        int maxInt = 0;
+        List<List<Object>> currentCContent = spreadSheet.getContents("C:C");
+        for (List<Object> data : currentCContent) {
+            for (Object datum : data) {
+                String strValue = String.valueOf(datum);
+                if (strValue.length() > 0) {
+                    try {
+                        int intVal = Integer.parseInt(strValue);
+                        if (maxInt < intVal) {
+                            maxInt = intVal;
+                        } else {
+                            continue;
+                        }
+                    } catch (NumberFormatException nfe) {
+                        continue;
+                    }
+                }
+            }
+        }
+        // 가져온 값 더하기 1을 한다.
+        maxInt += 1;
 
-    //     if (isInserted) {
-    //         System.out.println("성공적으로 데이터를 입력했습니다.");
-    //     } else {
-    //         System.out.println("실패");
-    //     }
-    //     return "ok";
-    // }
+        // 가져온 데이터를 엑셀 시트에 기입한다.
+        List<Object> dataSet = Arrays
+                .asList(calendar.getPreviousWeekMonday(false),
+                        calendar.getPreviousWeekSunday(false),
+                        String.valueOf(maxInt), revenue,
+                        newCustomers, newPurchasers, newRepurchasers,
+                        totalOrders, totalMargins);
+        boolean isInserted = spreadSheet.insertRow(dataSet);
+
+        if (isInserted) {
+            System.out.println("성공적으로 데이터를 입력했습니다.");
+        } else {
+            System.out.println("실패");
+        }
+    }
 }
